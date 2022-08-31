468187ef6eae88973b9a9c0a9470803fdd39013c
RohanKumarMainali <rohanmainali39@gmail.com>
2022-08-31 17:25:25 +0545

    index changes

--- controllers/feedbackControllers/sub-controller/postFeedBack.controller.js
+++ controllers/feedbackControllers/sub-controller/postFeedBack.controller.js
@@ -6,34 +6,32 @@ const PostFeedback = async (req, res) => {
   const { title, description, image, author } = req.body;
 
   //check if all attributes are recieved or not ?
-//   if (Object.keys(req.body).length < 4) {
-//     return res
-//       .status(StatusCodes.PARTIAL_CONTENT)
-//       .send("Some fields are missing. Please provide all the fields !!");
-//   }
+  //   if (Object.keys(req.body).length < 4) {
+  //     return res
+  //       .status(StatusCodes.PARTIAL_CONTENT)
+  //       .send("Some fields are missing. Please provide all the fields !!");
+  //   }
 
+  try {
+    const data = new routineModel({
+      title: title,
+      description: description,
+      image: req.file.path,
+      author: author,
+      createdOn: new Date().toLocaleDateString(),
+    });
 
-    try {
-      const data = new routineModel({
-        title: title,
-        description: description,
-        image: req.file.path,
-        author: author,
-        createdOn: new Date().toLocaleDateString(),
-      });
-
-      data.save();
-      return res.status(StatusCodes.ACCEPTED).send({
-        success: true,
-        message: "feedback posted successfully"
-      })
-    } catch (err) {
-      return res.status(500).send({
-        success: "fail",
-        message: err.message,
-      });
-    }
-  
+    data.save();
+    return res.status(StatusCodes.ACCEPTED).send({
+      success: true,
+      message: "feedback posted successfully",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      success: "fail",
+      message: err.message,
+    });
+  }
 };
 
 module.exports = PostFeedback;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,19 +1,15 @@
 const Router = require("express").Router();
 
 // Import user define routes
-// const GetBlog = require("./blog.routes")
 const BLOG_ROUTES = require("./blog.routes")
 const FEEDBACK_ROUTES = require("./feedback.routes");
-
-const PORTFOLIO_ROUTES = require("./portfolio.routes");
+// const PORTFOLIO_ROUTES = require("./portfolio.routes");
 const PROJECT_ROUTES = require("./project.routes");
 
 // Router.use(GetBlog);
 
 Router.use(FEEDBACK_ROUTES);
-
-Router.use(PORTFOLIO_ROUTES);
+// Router.use(PORTFOLIO_ROUTES);
 Router.use(PROJECT_ROUTES);
-
 
 module.exports = Router;
