2550f331349504addb35804d45e00eb1333fd818
RohanKumarMainali <rohanmainali39@gmail.com>
2022-08-31 15:40:52 +0545

    project and feedback completed

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,12 +5,12 @@ const Router = require("express").Router();
 const BLOG_ROUTES = require("./blog.routes")
 const FEEDBACK_ROUTES = require("./feedback.routes");
 // const PORTFOLIO_ROUTES = require("./portfolio.routes");
-// const PROJECT_ROUTES = require("./project.routes");
+const PROJECT_ROUTES = require("./project.routes");
 
 // Router.use(GetBlog);
 
 Router.use(FEEDBACK_ROUTES);
 // Router.use(PORTFOLIO_ROUTES);
-// Router.use(PROJECT_ROUTES);
+Router.use(PROJECT_ROUTES);
 
 module.exports = Router;
