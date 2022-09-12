c3ff8411f3bd5590e127cf781f30eca4532ba132
Samishan11 <samishanthapa0@gmail.com>
2022-09-12 12:23:03 +0545

    gallery compleyte

--- controllers/feedbackControllers/sub-controller/editFeedBack.controller.js
+++ controllers/feedbackControllers/sub-controller/editFeedBack.controller.js
@@ -3,49 +3,50 @@ const { StatusCodes } = require("http-status-codes");
 const cloudinary = require('../../../config/cloudinary');
 
 
-const EditFeedback = (req, res) => {
+const EditFeedback = async (req, res) => {
   //get the routine doc id
 
   const file = req?.files?.image
-  const result = cloudinary.uploader.upload(file.tempFilePath, { folder: 'blog', }, function (err, docs) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(docs)
-    }
-  })
   const { feedBackId, title, description, author } = req?.body;
-
-  // checking if feedBackId is in req.body
-  if (feedBackId === undefined)
-    return res.status(StatusCodes.NOT_ACCEPTABLE).send({
-      message: "feedBack ID is empty"
-    });
-
-  feedbackModel.findByIdAndUpdate(
-    feedBackId,
-    {
-      title: title,
-      description: description,
-      image: {
-        public_id: result.public_id,
-        url: result.secure_url
-      },
-      author: author,
-      createdOn: new Date().toLocaleDateString(),
-    },
-    (err, data) => {
+  try {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, { folder: 'blog', }, function (err, docs) {
       if (err) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-          message: 'Internal Server Error !!',
-        })
+        console.log(err)
       } else {
-        return res.status(StatusCodes.CREATED).send({
-          message: 'Feedback succesfully updated !!',
-        })
+        console.log(docs)
       }
-    }
-  )
+    })
+    // checking if feedBackId is in req.body
+    if (feedBackId === undefined)
+      return res.status(StatusCodes.NOT_ACCEPTABLE).send({
+        message: "feedBack ID is empty"
+      });
+
+    await feedbackModel.findByIdAndUpdate(
+      feedBackId,
+      {
+        title: title,
+        description: description,
+        image: {
+          public_id: result.public_id,
+          url: result.secure_url
+        },
+        author: author,
+        createdOn: new Date().toLocaleDateString(),
+      }
+
+    )
+    return res.status(StatusCodes.ACCEPTED).send({
+      message: 'Feedback succesfully updated !!',
+    })
+
+  } catch (error) {
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      message: 'Internal Server Error !!',
+    })
+
+  }
 }
 
 module.exports = EditFeedback;
